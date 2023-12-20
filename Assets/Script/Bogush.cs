361dd4e0ef14e3f0a6d64c0ad17bcfbf6a16dcf6
KayChiken <kainov.dmitry370@yandex.ru>
2023-12-20 16:14:56 +0300

    Animation Bogush

--- Assets/Script/Bogush.cs
+++ Assets/Script/Bogush.cs
@@ -10,16 +10,26 @@ public class Bogush : MonoBehaviour
     private bool temp = true;
     public int damage;
     private NavMeshAgent agent;
+    private Animator animator;
     public RectTransform healthBar;
     private void Start()
     {
         player = GameObject.Find("Player");
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
     }
     private void Update()
     {
+        float horizontal = Mathf.Clamp(agent.velocity.normalized.x, -1f, 1f);
+        float vertical = Mathf.Clamp(agent.velocity.normalized.z, -1f, 1f);
+        float speed = agent.velocity.magnitude;
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+        animator.SetFloat("speed" , speed);
+
         if (Vector3.Distance(transform.position, player.transform.position) <= 10 && Vector3.Distance(transform.position, player.transform.position) > 5)
         {
             transform.GetChild(0).gameObject.SetActive(false);
